874bf6686fe3877bc165dfaf9818f01731f5d1e7
dur <707475267@qq.com>
2015-08-28 18:09:02 +0800

    aaaaaa

---
+++ geiBit.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main(void){
+	float source;
+	unsigned long data;
+	char ch[34];
+	source = (float)0.75;
+	memcpy(&data, &source, 4);
+	printf("%lu\n",data);
+	for(int i = 33;i >= 0;i--){
+		if(i== 11 || i== 1){
+			ch[i] = '-';
+		}else{
+			ch[i] = (data % 2) == 1 ? '1' : '0';
+			data /=2;
+		}
+	}
+	printf("%s\n",ch);
+}
+
